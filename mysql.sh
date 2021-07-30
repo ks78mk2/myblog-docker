d9f874a3792d5f6da266dfc91b22f6c2c75d8fc4
jskim <ks78mk2@naver.com>
2021-07-30 15:00:22 +0900

    mysql.sh 수정

--- mysql.sh
+++ mysql.sh
@@ -4,4 +4,6 @@ if [ ! -d "$BASIC_PATH" ]; then
         echo "THIS BASIC PATH NOT EXIST"
         mkdir -p $BASIC_PATH
         mkdir -p $BASIC_PATH/data
+else 
+        echo "THIS BASIC PATH EXIST"
 fi
